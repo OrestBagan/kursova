d250954d0a8cf06553aa699641f6b133fcc9825b
OrestBagan <orestbagan2233@gmail.com>
2025-11-19 02:02:26 +0200

    fixed problems

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def menu_main(message):
 
 @bot.callback_query_handler(func=lambda call: call.data in ["coffe", "drinks", "desserts", "Меню"])
 def menu(call):
+    bot.answer_callback_query(call.id)
     if call.data == "coffe":
         markup_coffe = telebot.types.InlineKeyboardMarkup()
         in_keybord_coffe = {"Еспресо": "espreso", "Американо": "americano", "Капучино": "capuchino", "Лате": "late", "Назад": "Меню"}
@@ -38,22 +39,6 @@ def menu(call):
         for key in in_keybord_coffe.keys():
             button = telebot.types.InlineKeyboardButton(text=key, callback_data=in_keybord_coffe[key])
             markup_coffe.add(button)
-
-        markup_drinks = telebot.types.InlineKeyboardMarkup()
-        in_keybord_drinks = {"Чай зелений": "tea_green", "Чай чорний": "tea_black", "Матча": "matcha", "Какао": "kackao", "Назад": "Меню"}
-
-        for key in in_keybord_drinks.keys():
-            button = telebot.types.InlineKeyboardButton(text=key, callback_data=in_keybord_drinks[key])
-            markup_drinks.add(button)
-
-        markup_desserts = telebot.types.InlineKeyboardMarkup()
-        in_keybord_desserts = {"Чізкей": "cheesecake", "Тірамісу": "tiramisu ", "Макарони": "macaron", "Назад": "Меню"}
-
-        for key in in_keybord_desserts.keys():
-            button = telebot.types.InlineKeyboardButton(text=key, callback_data=in_keybord_desserts[key])
-            markup_desserts.add(button)
-
-
 
         bot.send_message(call.message.chat.id, """Ось доступні варіанти кави із цінами:\n
 • Еспресо — 35₴  
@@ -63,6 +48,13 @@ def menu(call):
 Добавити в корзину:""", reply_markup=markup_coffe)
         
     elif call.data == "drinks":
+        markup_drinks = telebot.types.InlineKeyboardMarkup()
+        in_keybord_drinks = {"Чай зелений": "tea_green", "Чай чорний": "tea_black", "Матча": "matcha", "Какао": "kackao", "Назад": "Меню"}
+
+        for key in in_keybord_drinks.keys():
+            button = telebot.types.InlineKeyboardButton(text=key, callback_data=in_keybord_drinks[key])
+            markup_drinks.add(button)
+
         bot.send_message(call.message.chat.id, """Ось доступні варіанти наопоїв із цінами:\n
 • Чай чорний/зелений — 35₴  
 • Матча — 70₴  
@@ -70,6 +62,13 @@ def menu(call):
 Добавити в корзину:""", reply_markup=markup_drinks)
         
     elif call.data == "desserts":
+        markup_desserts = telebot.types.InlineKeyboardMarkup()
+        in_keybord_desserts = {"Чізкей": "cheesecake", "Тірамісу": "tiramisu ", "Макарони": "macaron", "Назад": "Меню"}
+
+        for key in in_keybord_desserts.keys():
+            button = telebot.types.InlineKeyboardButton(text=key, callback_data=in_keybord_desserts[key])
+            markup_desserts.add(button)
+            
         bot.send_message(call.message.chat.id, """Ось доступні варіанти дисертів із цінами:\n
 • Чізкейк — 80₴  
 • Тірамісу — 85₴  
@@ -81,7 +80,7 @@ def menu(call):
 
 @bot.callback_query_handler(func=lambda call: call.data in ["espreso", "americano", "capuchino", "late", "tea_green", "tea_black", "matcha", "kackao", "cheesecake", "tiramisu", "macaron"])
 def save_cart(call):
-    filename = "database.json"
+    filename = "bascet.json"
 
     prices = {
         "espreso": ("Еспресо", 35), "americano": ("Американо", 40), 
@@ -105,11 +104,13 @@ def save_cart(call):
     user_cart = data.get(str(call.from_user.id), [])
     
     found_item = None
-    for item in user_cart:
-        if item.get("id") == call.data:
-            found_item = item
+    corent_quontity = 0
+    for i in user_cart:
+        if i.get("id") == call.data:
+            found_item = i
+            corent_quontity = i.get("quantity")
             break
-            
+    
     if found_item:
         found_item["quantity"] = found_item.get("quantity", 0) + 1
     else:
@@ -125,7 +126,11 @@ def save_cart(call):
 
     with open(filename, "w", encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-    
-    bot.send_message(call.message.chat.id, "Торва додано до корзини!")
+
+    bot.answer_callback_query(
+        call.id,
+        text=f"✅ {prices[call.data][0]} додано! Кількість: {corent_quontity + 1}", 
+        show_alert=False
+    )
 
 bot.polling()
